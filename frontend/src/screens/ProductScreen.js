cd5fe0c064d2d7196b57799a0cad3b7538a2cb87
sone <sthaya@gmail.com>
2021-10-09 19:56:31 -0700

    added cart screen and add to cart function

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -15,7 +15,7 @@ import Loader from "../components/Loader";
 import Message from "../components/Message";
 import { listProductDetails } from "../actions/productActions";
 
-const ProductScreen = ({ match }) => {
+const ProductScreen = ({ match, history }) => {
   const [qty, setQty] = useState(1);
 
   const dispatch = useDispatch();
@@ -27,6 +27,10 @@ const ProductScreen = ({ match }) => {
   useEffect(() => {
     dispatch(listProductDetails(match.params.id));
   }, [dispatch, match.params.id]);
+
+  const addToCartHandler = () => {
+    history.push(`/cart/${match.params.id}?qty=${qty}`);
+  };
 
   return (
     <div>
@@ -108,6 +112,7 @@ const ProductScreen = ({ match }) => {
                 <ListGroup.Item>
                   <Row>
                     <Button
+                      onClick={addToCartHandler()}
                       className="btn-block"
                       disabled={product.countInStock === 0}
                       type="button"
