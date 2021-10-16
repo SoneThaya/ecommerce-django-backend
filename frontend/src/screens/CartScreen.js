71a4032914b297dd0220e4c66a440efc2db48afd
sone <sthaya@gmail.com>
2021-10-15 20:40:20 -0700

    setup cart screen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -42,10 +42,26 @@ const CartScreen = ({ match, location, history }) => {
               <ListGroup key={item.product}>
                 <Row>
                   <Col md={2}>
-                    <Image src={item.image} />
+                    <Image src={item.image} alt={item.name} fluid rounded />
                   </Col>
                   <Col md={3}>
                     <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>${item.price}</Col>
+                  <Col>
+                    <Form.Control
+                      as="select"
+                      value={item.qty}
+                      onChange={(e) =>
+                        dispatch(addToCart(item.product, e.target.value))
+                      }
+                    >
+                      {[...Array(item.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
                   </Col>
                 </Row>
               </ListGroup>
